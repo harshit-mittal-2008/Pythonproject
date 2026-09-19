2ccda6fbb182ef3010875a4a4ecaf43ced5043de
Harshit <harsitmittal@gmail.com>
2026-09-19 13:47:09 +0530

    upper_triangle(Method - 2)

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-for i in range(5, 0, -1):
-    for j in range(1, i + 1):
-        print("*", end="  ")
+for i in range(1, 6):
+    for j in range(1, 6):
+        if j >= i:
+            print("*", end="  ")
     print()
